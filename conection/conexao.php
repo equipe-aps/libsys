ce5c6dde7526c0867f88ecfc045f284d7ffc64bd
Gustavo <gustavo.soares.cdc@gmail.com>
2018-09-30 14:54:01 -0300

    commit-30/09/2018

--- conection/conexao.php
+++ conection/conexao.php
@@ -1,13 +1,13 @@
 <?php
 error_reporting (E_ALL & ~ E_NOTICE & ~ E_DEPRECATED);
-$con = mysql_connect("localhost", "root", "");
-$db = mysql_select_db("server", $con);
+$con = mysql_connect("mysql.hostinger.com.br", "u923371810_admin", "retiroartes");
+$db = mysql_select_db("u923371810_grupo", $con);
 if (!$con) {
 	die("Não foi possível  conectar ao Banco de Dados: " . mysql_error());
 }
-mysql_query("SET NAME 'utf8'");
-mysql_query("SET character_set_conection=utf8");
-mysql_query("SET character_set_client=utf8");
-mysql_query("SET character_set_result=utf8");
+mysql_query("SET NAME 'utf8");
+mysql_query('SET character_set_conection=utf8');
+mysql_query('SET character_set_client=utf8');
+mysql_query('SET character_set_result=utf8');
 
 ?>
